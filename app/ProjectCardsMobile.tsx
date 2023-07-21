b03bf42a1dc7b60e2562384a02d2daaea3165dfc
Pierre Amelot <pierre.p.amelot@gmail.com>
2023-07-21 14:00:04 -0400

    Fix fake project mobile nav for testing

--- app/ProjectCardsMobile.tsx
+++ app/ProjectCardsMobile.tsx
@@ -1,17 +1,31 @@
+import Link from 'next/link';
+
 export function ProjectCardsMobile({ className }: { className?: string }) {
   return (
     <ul className={`flex flex-col gap-8  bg-bg-surface ${className}`}>
       <li>
-        <div className="h-[20rem] w-full rounded-md bg-red-200" />
+        <Link
+          href="/project/journey-finder"
+          className="block h-[20rem] w-full rounded-md bg-red-200"
+        />
       </li>
       <li>
-        <div className="h-[20rem] w-full rounded-md bg-red-200" />
+        <Link
+          href="/project/journey-finder"
+          className="block h-[20rem] w-full rounded-md bg-red-200"
+        />
       </li>
       <li>
-        <div className="h-[20rem] w-full rounded-md bg-red-200" />
+        <Link
+          href="/project/journey-finder"
+          className="block h-[20rem] w-full rounded-md bg-red-200"
+        />
       </li>
       <li>
-        <div className="h-[20rem] w-full rounded-md bg-red-200" />
+        <Link
+          href="/project/journey-finder"
+          className="block h-[20rem] w-full rounded-md bg-red-200"
+        />
       </li>
     </ul>
   );
